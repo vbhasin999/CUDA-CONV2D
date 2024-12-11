f9414ad22e1e4ff568f77b225d7354ab71b52574
vbhasin999 <vbhasin999@gmail.com>
2024-12-10 21:09:29 -0500

    dynamic smem kernel

--- conv2d/src/conv2d_impl.cu
+++ conv2d/src/conv2d_impl.cu
@@ -4,118 +4,14 @@
 #define BLOCK_DIM_X 16
 #define BLOCK_DIM_Y 16
 #define BLOCK_DIM_Z 2
+#define PIXELS_PER_THREAD 16
 
 // Add batch, stride and padding later
 // we could have 1 block <-> 1 output channel
 // and 1 thread <-> 1 output pixel
-template <typename T>
-__global__ void conv_kernel_basic(T *result, const T *input, const T *filter, int Cin, int H, int W, int Cout, int K) {
-    int x = threadIdx.x;
-    int y = threadIdx.y;
-    int z = blockIdx.x;
-
-    int H_out = H - K + 1;
-    int W_out = W - K + 1;
-
-    T sum = 0;
-    if (x < H_out && y < W_out){
-        // loop over input channels
-        for (int i = 0; i < Cin; i++) {
-            // loop over filter dimensions
-            for (int j = 0; j < K; j++) {
-                for (int k = 0; k < K; k++) {
-                    // input of shape (Cin, H, W) with strides (H*W, W, 1)
-                    // filter of shape (Cout, Cin, K, K) with strides (Cin*K*K, K*K, K, 1)
-                    sum += input[i * H * W + (x + j) * W + (y + k)] * filter[z * Cin * K * K + i * K * K + j * K + k];
-                }
-            }
-        }
-        // result of shape (Cout, H_out, W_out) with strides (H_out * W_out, W_out, 1)
-        result[z * H_out * W_out + x * W_out + y] = sum;
-    }
-}
 
 template <typename T>
-void launch_conv2d_basic(T *h_result, const T *h_x, const T *h_y, 
-                   int Cin, int H, int W, int Cout, int K) {
-    // Output dimensions
-    int H_out = H - K + 1;
-    int W_out = W - K + 1;
-
-    // Device memory allocation
-    T *d_x, *d_y, *d_result;
-    size_t input_size = Cin * H * W * sizeof(T);
-    size_t filter_size = Cout * Cin * K * K * sizeof(T);
-    size_t output_size = Cout * H_out * W_out * sizeof(T);
-
-    cudaMalloc(&d_x, input_size);
-    cudaMalloc(&d_y, filter_size);
-    cudaMalloc(&d_result, output_size);
-
-    // Copy inputs to device
-    cudaMemcpy(d_x, h_x, input_size, cudaMemcpyHostToDevice);
-    cudaMemcpy(d_y, h_y, filter_size, cudaMemcpyHostToDevice);
-
-    // Define grid and block dimensions
-    dim3 threadsPerBlock(W_out, H_out); // one thread per output pixel
-    dim3 numBlocks(Cout);          // One block per output channel and input channel
-
-    // Launch kernel
-    conv_kernel_basic<<<numBlocks, threadsPerBlock>>>(d_result, d_x, d_y, Cin, H, W, Cout, K);
-    cudaDeviceSynchronize();
-    cudaError_t err = cudaGetLastError();
-    if (err != cudaSuccess) {
-        fprintf(stderr, "CUDA error: %s\n", cudaGetErrorString(err));
-    }
-
-    // Copy results back to host
-    cudaMemcpy(h_result, d_result, output_size, cudaMemcpyDeviceToHost);
-
-    // Cleanup
-    cudaFree(d_x);
-    cudaFree(d_y);
-    cudaFree(d_result);
-}
-
-// Supports batching
-// Z dim for batch
-// X dim for channel
-// Y dim for height, width
-// template <typename T>
-// __global__ void conv_kernel_batched(T *result, const T *input, const T *filter, int N, int Cin, int H, int W, int Cout, int Kh, int Kw) {
-//     int x = threadIdx.x; // output channel
-//     int y = threadIdx.y; // output pixel
-//     int z = threadIdx.z; // batch
-
-//     int H_out = H - Kh + 1;
-//     int W_out = W - Kw + 1;
-
-//     int t_r = y / W_out;
-//     int t_c = y % W_out;
-
-//     T sum = 0;
-
-//     if (x < Cout && y < H_out * W_out && z < N){
-//         for (int i = 0; i < Cin; ++i ){
-//             for (int j = 0; j < Kh; ++j){
-//                 for (int k = 0; k < Kw; ++k){
-//                     printf("z: %d, x: %d, y: %d, i: %d, j: %d, k: %d\n sum += %f * %f", z, x, y, i, j, k, input[(z * Cin * H * W) + (i * H * W) + ((t_r + j) * W) + (k + t_c) ], filter[(x * Cin * Kh * Kw) + (i * Kh * Kw) + (j * Kw) + k ]);
-//                     sum += input[(z * Cin * H * W) + (i * H * W) + ((t_r + j) * W) + (k + t_c) ] * filter[(x * Cin * Kh * Kw) + (i * Kh * Kw) + (j * Kw) + k ];
-    
-//                 }
-//             }
-//         }
-
-//         // result of shape N, Cout, Hout, Wout
-//         result[(z * Cout * H_out * W_out) + (x * H_out * W_out) + (t_r * W_out) + t_c] = sum;
-
-//     }
-    
-    
-// }
-
-template <typename T>
-__global__ void conv_kernel_opt(
+__global__ void conv_kernel_basic(
     T* __restrict__ result, 
     const T* __restrict__ input, 
     const T* __restrict__ filter, 
@@ -168,7 +64,7 @@ __global__ void conv_kernel_opt(
 
 
 template <typename T>
-void launch_conv2d_opt(T *h_result, const T *h_x, const T *h_y, int Cin, int H, int W, int Cout, int K) {
+void launch_conv2d_basic(T *h_result, const T *h_x, const T *h_y, int Cin, int H, int W, int Cout, int K) {
     // Output dimensions
     int H_out = H - K + 1;
     int W_out = W - K + 1;
@@ -203,7 +99,110 @@ void launch_conv2d_opt(T *h_result, const T *h_x, const T *h_y, int Cin, int H, 
                 Cout);   
 
     // Launch kernel
-    conv_kernel_opt<<<gridDim, blockDim>>>(d_result, d_x, d_y, Cin, H, W, Cout, K);
+    conv_kernel_basic<<<gridDim, blockDim>>>(d_result, d_x, d_y, Cin, H, W, Cout, K);
+    cudaDeviceSynchronize();
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "3. CUDA error: %s\n", cudaGetErrorString(err));
+    }
+
+    // Copy results back to host
+    cudaMemcpy(h_result, d_result, output_size, cudaMemcpyDeviceToHost);
+
+    // Cleanup
+    cudaFree(d_x);
+    cudaFree(d_y);
+    cudaFree(d_result);
+}
+
+template <typename T>
+__global__ void conv_kernel_opt(
+    T* __restrict__ result, 
+    const T* __restrict__ input, 
+    const T* __restrict__ filter, 
+    int Cin, int H, int W, int Cout, int K) {
+
+    int out_x = threadIdx.x + blockIdx.x * blockDim.x;  // Output width index
+    int out_y = threadIdx.y + blockIdx.y * blockDim.y;  // Output height index
+    int in_c = blockIdx.z;                            // Input channel index                 
+
+    int H_out = H - K + 1;
+    int W_out = W - K + 1;
+
+    // should be of size blockDim.x + K - 1 * blockDim.y + K - 1 i.e. 
+    // the receptive field of the block
+    extern __shared__ T sInput[];
+    if (threadIdx.z == 0) {
+        for (int i = 0; i < K; ++i) {
+            for (int j = 0; j < K; ++j) {
+                int in_x = out_x + i;
+                int in_y = out_y + j;
+                sInput[i * K + j] = (in_x < W && in_y < H) ? input[in_c * H * W + in_y * W + in_x] : 0.0f;
+            }
+        }
+    }
+
+    __syncthreads();
+
+    T local_sum = 0;
+    if (out_x < H_out && out_y < W_out) {
+        // Loop over output channels in chunks
+        for (int out_c = threadIdx.z; out_c < Cout; in_c += BLOCK_DIM_Z) {
+            for (int kx = 0; kx < K; ++kx) { // Kernel rows
+                for (int ky = 0; ky < K; ++ky) { // Kernel columns
+                    int in_x = out_x + kx;
+                    int in_y = out_y + ky;
+
+                    if (in_x < W && in_y < H) {  // Bounds check
+                        local_sum += sInput[kx * K + ky] *
+                                        filter[out_c * Cin * K * K + in_c * K * K + ky * K + kx];
+                    }
+                }
+            }
+            result[out_c * H_out * W_out + out_y * W_out + out_x] += local_sum;
+        }
+    }
+}
+
+template <typename T>
+void launch_conv2d_opt(T *h_result, const T *h_x, const T *h_y, int Cin, int H, int W, int Cout, int K) {
+    // Output dimensions
+    int H_out = H - K + 1;
+    int W_out = W - K + 1;
+
+    // Device memory allocation
+    T *d_x, *d_y, *d_result;
+    size_t input_size = Cin * H * W * sizeof(T);
+    size_t filter_size = Cout * Cin * K * K * sizeof(T);
+    size_t output_size = Cout * H_out * W_out * sizeof(T);
+
+    cudaMalloc(&d_x, input_size);
+    cudaMalloc(&d_y, filter_size);
+    cudaMalloc(&d_result, output_size);
+    cudaError_t err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "1. CUDA error: %s\n", cudaGetErrorString(err));
+    }
+
+    // Copy inputs to device
+    cudaMemcpy(d_x, h_x, input_size, cudaMemcpyHostToDevice);
+    cudaMemcpy(d_y, h_y, filter_size, cudaMemcpyHostToDevice);
+
+    err = cudaGetLastError();
+    if (err != cudaSuccess) {
+        fprintf(stderr, "2. CUDA error: %s\n", cudaGetErrorString(err));
+    }
+
+    // Define grid and block dimensions
+    dim3 blockDim(BLOCK_DIM_X, BLOCK_DIM_Y, BLOCK_DIM_Z);  
+    dim3 gridDim((W_out + blockDim.x - 1) / blockDim.x,
+                (H_out + blockDim.y - 1) / blockDim.y,
+                Cin);   
+
+    size_t shared_mem_size = (BLOCK_DIM_X + K - 1) * (BLOCK_DIM_Y + K - 1) * sizeof(T);
+
+    // Launch kernel
+    conv_kernel_basic<<<gridDim, blockDim, shared_mem_size>>>(d_result, d_x, d_y, Cin, H, W, Cout, K);
     cudaDeviceSynchronize();
     err = cudaGetLastError();
     if (err != cudaSuccess) {
